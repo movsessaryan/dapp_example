748bddf409f31e672aec957391139e82a8c913cf
Movses Saryan <movsessaryan@gmail.com>
2017-10-30 17:14:46 +0400

    fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,12 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
-
+var Validator = artifacts.require("./Validator.sol");
+var Offer = artifacts.require("./Offer.sol");
 module.exports = function(deployer) {
+  deployer.deploy(Offer);
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+  deployer.deploy(Validator);
+//  console.log(`>>>>>adress is${Offer.address}`)
 };
